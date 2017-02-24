0bd54d76cce0a9f2ea9ab011e2661c866b79bf9e
b5710546232 <nattapat.su@ku.th>
2017-02-24 10:09:44 +0700

    first commit

---
+++ providers/connectivity-service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Network } from 'ionic-native';
+import { Platform } from 'ionic-angular';
+
+
+declare var Connection;
+/*
+  Generated class for the ConnectivityService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class ConnectivityService {
+
+  onDevice: boolean;
+  constructor(public http: Http, public platform: Platform) {
+    console.log('Hello ConnectivityService Provider');
+    this.onDevice = this.platform.is('cordova');
+  }
+
+  isOnline(): boolean {
+    if (this.onDevice && Network.type) {
+      return Network.type !== Connection.NONE;
+    } else {
+      return navigator.onLine;
+    }
+  }
+  
+  isOffline(): boolean {
+    if (this.onDevice && Network.type) {
+      return Network.type === Connection.NONE;
+    } else {
+      return !navigator.onLine;
+    }
+  }
+
+}
